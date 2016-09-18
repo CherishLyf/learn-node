0906613b722ee7725c1cfe2b025279e6cadb1596
Lyf <517209885@qq.com>
2016-09-19 01:24:10 +0800

    learn node

--- lesson4: 服务端的模块/index.js
+++
@@ -1,4 +0,0 @@
-// 通常我们都会有一个 index.js 文件去调用其他应用的其他模块
-var server = require('./server')
-
-server.start()
--- lesson5: 如何来进行请求路由/router.js
+++
@@ -1,8 +0,0 @@
-
-// 添加路由模块
-
-function route(pathname) {
-  console.log('About to route a request for ' + pathname)
-}
-
-exports.route = route
